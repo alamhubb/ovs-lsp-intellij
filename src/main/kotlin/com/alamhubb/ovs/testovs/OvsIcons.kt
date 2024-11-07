815ffb8fdcd31ec60e0d06241e5e051fabcf9a81
alamhubb <alamhubb@163.com>
2024-11-07 16:26:02 +0800

    simple改名ovs

--- src/main/kotlin/com/alamhubb/ovs/testovs/OvsIcons.kt
+++ src/main/kotlin/com/alamhubb/ovs/testovs/OvsIcons.kt
@@ -6,4 +6,5 @@ import javax.swing.Icon
 object OvsIcons {
     //  public static final Icon FILE = IconLoader.getIcon("/icons/ovs.png", com.alamhubb.ovs.testovs.SimpleIcons.class);
     val FILE: Icon = getIcon("/icons/ovs.png", OvsIcons::class.java)
+//    val FILE: Icon = getIcon("/icons/fasdfs.png", OvsIcons::class.java)
 }
