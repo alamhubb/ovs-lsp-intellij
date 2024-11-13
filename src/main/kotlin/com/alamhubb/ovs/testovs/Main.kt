33a0ca419fead74f95c4add654d5ec3e1cffd057
alamhubb <alamhubb@163.com>
2024-11-13 21:21:04 +0800

    支持ovs

--- src/main/kotlin/com/alamhubb/ovs/testovs/Main.kt
+++ src/main/kotlin/com/alamhubb/ovs/testovs/Main.kt
@@ -1,7 +1,26 @@
-import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+interface Printer {
+    var name: String
+    fun print()
+}
+
+object ConsolePrinter : Printer {
+    override var name = "console"
+    override fun print() {
+        println("Printing to $name")
+    }
+}
+
+// 使用委托
+object Document : Printer by ConsolePrinter {
+    fun install() {
+        println("Installing $name")
+    }
+}
 
 fun main() {
-    val name = "IDENTIFIER"
-    println(DefaultLanguageHighlighterColors::class.java.getField(name))
-    // 因为是 public static final 字段，使用 getField
+    Document.install()  // 输出: Installing console
+    Document.name = "Document"
+    Document.print()    // 输出: Printing to Document
+
+    ConsolePrinter.print() // 输出: Printing to Document
 }
--- src/main/kotlin/com/alamhubb/ovs/testovs/OvsLspServerSupportProvider.kt
+++ src/main/kotlin/com/alamhubb/ovs/testovs/OvsLspServerSupportProvider.kt
@@ -7,10 +7,13 @@ import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.lsp.api.LspServerDescriptor
 import com.intellij.platform.lsp.api.LspServerSupportProvider
 import com.intellij.platform.lsp.api.LspServerSupportProvider.LspServerStarter
 import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor
+import com.intellij.platform.lsp.api.customization.LspCompletionSupport
 import com.intellij.platform.lsp.api.customization.LspSemanticTokensSupport
+import org.eclipse.lsp4j.CompletionItem
 import java.awt.Color
 import java.awt.Font
 
@@ -23,19 +26,28 @@ class OvsLspServerSupportProvider : LspServerSupportProvider {
     }
 }
 
-private class FooLspServerDescriptor(project: Project) : ProjectWideLspServerDescriptor(project, "Ovs") {
+private class FooLspServerDescriptor(project: Project) : LspServerDescriptor(project, "Ovs") {
+
+    override val lspCompletionSupport: LspCompletionSupport?
+        get() = object : LspCompletionSupport() {
+            // 你可以在此处定义该属性的自定义实现
+            // 重写 getTailText 方法
+            override fun getTailText(item: CompletionItem): String? = item.labelDetails?.detail
+
+        }
+
     override fun isSupportedFile(file: VirtualFile) = file.extension == "ovs"
     override fun createCommandLine(): GeneralCommandLine {
         val path = System.getenv("PATH")
         println("Current PATH: $path")
 //        return GeneralCommandLine("tsx.cmd", "D:/project/subhutiall/ovs-lsp/src/index.ts", "--stdio")
-//        return GeneralCommandLine("tsx.cmd", "E:/qkyproject/ovsall/ovs-starter/src/index.ts", "--stdio")
-        return GeneralCommandLine("tsx.cmd", "E:/qkyproject/subhutiall/ovs-language-server/src/index.ts", "--stdio")
+        return GeneralCommandLine(
+            "tsx.cmd",
+            "E:/qkyproject/ovsall/ovs-starter/packages/language-server/src/index.ts",
+            "--stdio"
+        )
+//        return GeneralCommandLine("tsx.cmd", "E:/qkyproject/subhutiall/ovs-language-server/src/index.ts", "--stdio")
 //        return GeneralCommandLine("tsx", "/Users/qinky/WebstormProjects/subhutiall/ovs-lsp/src/index.ts", "--stdio")
-            .apply {
-                withCharset(Charsets.UTF_8)
-                withRedirectErrorStream(true)
-            }
 //        return GeneralCommandLine("node", "console.log(123)", "ovs-lsp", "echo", "--stdio")
     }
 
